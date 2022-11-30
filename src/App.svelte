55437584bb257ba4ec10b61018261ab77ae2b9d1
CodeSandbox <support@codesandbox.io>
2022-11-30 17:00:02 +0000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div class="">
+    <table class="">
+        <thead class="">
+            <tr class="">
+                <th class="font-bold">invoice</th>
+            </tr>
+        </thead>
+        <tbody class="">
+            <tr class="">
+                <td class="">No</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
